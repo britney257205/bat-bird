eaab26da7432a0787739a8c1bd6acb331427dcb7
britney257205 <78146127+britney257205@users.noreply.github.com>
2021-06-08 10:18:27 +0200

    utilities

---
+++ utilities.lua
@@ -0,0 +1,58 @@
+
+local GGData = require("GGData")
+
+
+
+local utilities = {}
+local db = GGData:new("db")
+
+
+
+
+if not db.highscore then
+  db:set("highscore", 0)
+  db:save()
+end
+
+if not db.previousScore then
+    db:set("previousScore", 0)
+    db:save()
+end
+
+
+
+
+function utilities:getHighscore()
+    return db.highscore
+end
+
+function utilities:getPreviousScore()
+    return db.previousScore
+end
+
+function utilities:setHighScore(score)
+    if score > db.highscore then
+        print("New highscore")
+
+        db:set("highscore", score)
+        db:save()
+
+        return true
+    else
+        return false
+    end
+end
+
+function utilities:setPreviousScore(score)
+    db:set("previousScore", score)
+    db:save()
+end
+
+
+
+
+-- -----------------------------------------------------------------------------------
+-- Return
+-- -----------------------------------------------------------------------------------
+
+return utilities
